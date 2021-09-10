465a51ff779f763ee4dfab67e05060f76bf33b15
DonSera <sera.2021@lifeoasis.com>
2021-09-10 15:41:47 +0900

    feat : 아이콘 추가하기 (폴더모양)

--- src/component/CatFolder/CatFolder.js
+++ src/component/CatFolder/CatFolder.js
@@ -1,32 +1,30 @@
 import styles from './CatFolder.module.css'
+import {AiOutlineFolder} from "react-icons/ai";
 
-function CatFolder({id, name, type, filePath}) {
+function CatFolder({name, type, filePath}) {
     const imgUrl = `https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public`
 
-    function imgCase(){
+    function imgCase() {
         return (
-            <div>
-                <img src={imgUrl+filePath} alt={`이미지`} width={"200px"} height={"200px"}/>
+            <div className={styles['img-card']}>
+                <img src={imgUrl + filePath} alt={`이미지`} className={styles['img']}/>
                 <p>{name}</p>
-                <p>id : {id}</p>
             </div>
         );
     }
 
-    function folderCase(){
-        return(
+    function folderCase() {
+        return (
             <div>
-                <button>{type}</button>
+                <AiOutlineFolder size={"200px"} color={"black"}/>
                 <p>{name}</p>
-                <p>id : {id}</p>
             </div>
         )
     }
 
-    function render(){
-        console.log(`${type} : ${name}`)
-        if(type === "DIRECTORY") return folderCase();
-        else if (type === "FILE") return  imgCase();
+    function render() {
+        if (type === "DIRECTORY") return folderCase();
+        else if (type === "FILE") return imgCase();
         else return <div>Not Dic and File</div>
     }
 
