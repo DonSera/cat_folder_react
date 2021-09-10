e0030df7188f1b9cca6e9992839e1cc84a07498d
DonSera <sera.2021@lifeoasis.com>
2021-09-10 16:46:21 +0900

    feat : react-loader-spinner 구현

---
+++ src/component/Loading/Loading.js
@@ -0,0 +1,15 @@
+import styles from './Loading.module.css'
+import Loader from "react-loader-spinner";
+
+function Loading() {
+    const loader = <Loader type="Oval"
+                           color="#000000"
+                           height={"100%"}
+                           width={"100%"}
+                           timeout={"infinity"}/>;
+    const loaderText = <span className={styles['text']}>Loading ...</span>
+
+    return <div className={styles['loading-div']}>{loader}{loaderText}</div>
+}
+
+export default Loading;
