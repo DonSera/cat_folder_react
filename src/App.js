b0589b034265aa0ad34748fff4b895c70bce7e88
DonSera <sera.2021@lifeoasis.com>
2021-09-10 13:56:26 +0900

    feat : type이 FILE일 경우 이미지로 보여주기

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
 import "./App.css";
 import CatFolder from "./component/CatFolder";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import {useEffect, useRef, useState} from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function App() {
-  const parentId = useRef(``);
-  const localId = useRef('');
-  const [load, setLoad] = useState(false);
-  const [folderInfo, setFolderInfo] = useState([]);
+    const [load, setLoad] = useState(false);
+    const [folderInfo, setFolderInfo] = useState([]);
 
-  useEffect(() => {
-    console.log(`useEffect 실행`)
-    saveFolderInfo("");
-  }, []);
+    useEffect(() => {
+        console.log(`useEffect 실행`)
 
-  async function saveFolderInfo(id) {
-    setLoad(true);
-    console.log(`들어온 id : ${id}`);
-    parentId.current = localId.current;
-    localId.current = id;
-    const folderInfoArray = await getJson(id);
-    // {"id":"1","name":"노란고양이","type":"DIRECTORY","filePath":null,"parent":null}
-
-    console.log(folderInfoArray);
-    setFolderInfo(folderInfoArray);
-    setLoad(false);
-  }
+        saveFolderInfo("2").then(() => {
+            console.log("useEffect 끝");
+        });
+    }, []);
 
 
-  async function getJson(id) {
-    // API를 이용하여 json 결과 array로 불러오기
-    console.log(`start fetch`);
-    const url = `https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev/${id}`;
-    const folderInfoFetch = await fetch(url);
-    console.log(`end fetch`);
-    return await folderInfoFetch.json();
-  }
+    async function saveFolderInfo(id) {
+        console.log(`들어온 id : ${id}`);
 
+        // 로딩 실행
+        setLoad(true);
 
-  function renderMain() {
-    if (load) {
-      return <div>Loading</div>;
-    } else {
-      const folders = folderInfo.map(obj => {
-        return <CatFolder id={obj.id}
-                          name={obj.name}
-                          type={obj.type}
-                          filePath={obj.filePath}/>
-      })
+        // [{"id":"1","name":"노란고양이","type":"DIRECTORY","filePath":null,"parent":null},...]
+        // 결과 정보 저장
+        const infoArray = await getJson(id);
+        setFolderInfo(infoArray);
 
-      // const backButton = <button onClick={() => saveFolderInfo(parentId.current)}>뒤로가기</button>
-
-      return <Router>{folders}</Router>;
+        // 로딩 끝
+        setLoad(false);
     }
-  }
 
-  return (
-      <div className={`App`}>
-        <header className={`App-header`}>
-          {renderMain()}
-        </header>
-      </div>
-  );
+
+    async function getJson(id) {
+        // API를 이용하여 json 결과 array로 불러오기
+        console.log(`start fetch`);
+        const url = `https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev/${id}`;
+        const folderInfoFetch = await fetch(url);
+        const folderInfoArray = await folderInfoFetch.json();
+        console.log(`end fetch`);
+        return folderInfoArray;
+    }
+
+
+    function renderMain() {
+        if (load) {
+            return <div>Loading</div>;
+        } else {
+            const catFolderComponent = folderInfo.map(obj => <CatFolder id={obj.id}
+                                                                        name={obj.name}
+                                                                        type={obj.type}
+                                                                        filePath={obj.filePath}/>);
+            return (catFolderComponent);
+        }
+    }
+
+    return (
+        <div className={`App`}>
+            <header className={`App-header`}>
+                {renderMain()}
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/component/CatFolder/CatFolder.js
+++ src/component/CatFolder/CatFolder.js
@@ -1,15 +1,38 @@
 import styles from './CatFolder.module.css'
 
 function CatFolder({id, name, type, filePath}) {
+    const imgUrl = `https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public`
+
+    function imgCase(){
+        return (
+            <div>
+                <img src={imgUrl+filePath} alt={`이미지`} width={"200px"} height={"200px"}/>
+                <p>{name}</p>
+                <p>id : {id}</p>
+            </div>
+        );
+    }
+
+    function folderCase(){
+        return(
+            <div>
+                <button>{type}</button>
+                <p>{name}</p>
+                <p>id : {id}</p>
+            </div>
+        )
+    }
+
+    function render(){
+        console.log(`${type} : ${name}`)
+        if(type === "DIRECTORY") return folderCase();
+        else if (type === "FILE") return  imgCase();
+        else return <div>Not Dic and File</div>
+    }
+
     return (
         <div>
-            {
-                type === "DIRECTORY" ?
-                    <button>{type}</button>
-                    : <img src={filePath} alt={`이미지`} width={"200px"} height={"200px"}/>
-            }
-            <p>{name}</p>
-            <p>id : {id}</p>
+            {render()}
         </div>
     );
 }
